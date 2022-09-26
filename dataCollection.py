d308639b5bc293124fc8ac2ddffacb8731d56472
Rishu Verma <dev.rishu28@gmail.com>
2022-09-26 13:32:56 -0400

    Analysis done on the observations

--- dataCollection.py
+++ dataCollection.py
@@ -3,7 +3,7 @@ import csv
 with open('Observations.csv', 'w', newline='') as file:
     header = ["FileName","Cache Size (bytes)", "Associativity", "Access Time (ns)","Dynamic read energy/access (nJ)","Area (mm2)"];
     writer = csv.writer(file)
-    writer.writerow(header);
+    writer.writerow(header)
 
     new_list = []
     idx = 0
@@ -12,7 +12,7 @@ with open('Observations.csv', 'w', newline='') as file:
     object = os.scandir(path_of_the_directory)
     for file in object :
         temp_list=[]
-        temp_idx = 1;
+        temp_idx = 1
         if file.is_file():
             temp_list.insert(0, file.name)
             with open(file, 'r') as fp:
@@ -25,10 +25,10 @@ with open('Observations.csv', 'w', newline='') as file:
                             #value = row.partition(word)[2]
                             temp_list.insert(idx, splitRow[len(splitRow) -1])
                             temp_idx =temp_idx+1
-                print(temp_list);
-        fp.close();
-        new_list.insert(idx,temp_list);
-        idx+=1;
+                print(temp_list)
+        fp.close()
+        new_list.insert(idx,temp_list)
+        idx+=1
 
 
     if len(new_list)==0:
@@ -37,6 +37,3 @@ with open('Observations.csv', 'w', newline='') as file:
         print (new_list)
         writer.writerows(new_list)
     object.close()
-
-
-                        # print it.
